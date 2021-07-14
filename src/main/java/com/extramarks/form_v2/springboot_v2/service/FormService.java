3830419d9369019cb8bf2af1823cd1231b6a30a9
UtsavRajVerma <verma.utsavr@gmail.com>
2021-07-14 11:22:59 +0530

    Revert "First"
    This reverts commit 051b64274277b7540c52238125496a07b162632a.
    :X

--- src/main/java/com/extramarks/form_v2/springboot_v2/service/FormService.java
+++ src/main/java/com/extramarks/form_v2/springboot_v2/service/FormService.java
@@ -33,13 +33,7 @@ public class FormService {
         existingForm.setGender(form.getGender());
         existingForm.setSkills(form.getSkills());
         existingForm.setState(form.getState());
-        existingForm.setImage(form.getImage());
 
         return formRepository.save(existingForm);
-    }
-
-    public Form saveFormFile(Form form, String url){
-        form.setImage(url);
-        return form;
     }
 }
